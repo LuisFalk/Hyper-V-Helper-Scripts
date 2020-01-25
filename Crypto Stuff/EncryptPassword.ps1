b9a3c4fa6aecc5e62b0dbb4cc3a6b777836bb684
Luis <46562912+LuisFalk@users.noreply.github.com>
2020-01-25 11:30:57 +0100

    Initial Upload

---
+++ Crypto Stuff/EncryptPassword.ps1
@@ -0,0 +1,6 @@
+﻿$PasswordFile = "encrypted.key"
+$KeyFile = "AES_KEY.key"
+$Key = Get-Content $KeyFile
+$pw = Read-Host 'Password to Encrypt:'
+$Password = $pw | ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
---
+++ Crypto Stuff/Generate_RandomKey.ps1
@@ -0,0 +1,4 @@
+﻿$KeyFile = "AES_KEY.key"
+$Key = New-Object Byte[] 16   # 16, 24, or 32 for AES
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | out-file $KeyFile
